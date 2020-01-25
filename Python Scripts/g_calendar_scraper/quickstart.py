7de83dd89b38d11bd88531140e005066a1183478
Nayman Leung <naymanl2@illinois.edu>
2020-01-25 14:00:30 -0600

    python open() doesn't have permission to create files or something, couldn't resolve it so pushing over to desktop to see if it works

--- Python Scripts/g_calendar_scraper/quickstart.py
+++ Python Scripts/g_calendar_scraper/quickstart.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from __future__ import print_function
 import datetime
 import pickle
@@ -9,7 +11,7 @@ from google.auth.transport.requests import Request
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
-def main():
+def main(fp):
     """Shows basic usage of the Google Calendar API.
     Prints the start and name of the next 10 events on the user's calendar.
     """
@@ -35,15 +37,21 @@ def main():
     service = build('calendar', 'v3', credentials=creds)
 
     # Call the Calendar API
-    today = datetime.datetime.now()
-    d = datetime.timedelta(days=7)  # want to grab on a weekly basis
-    a = today - d
-    start = a.isoformat() + 'Z'
+
+    
+    # today = datetime.datetime.now()
+    # d = datetime.timedelta(days=7)  # want to grab on a weekly basis
+    # a = today - d
+    # start = a.isoformat() + 'Z'
+
+    start, _ = week_magic()
+    start = start.isoformat() + 'Z'
+
     # now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
     # print('Getting the upcoming 10 events')
-    print('Getting past events since 6 days ago')
+    print('Getting past events from the beginning of the week')
     events_result = service.events().list(calendarId='primary', timeMin=start,
-                                        maxResults=10, singleEvents=True,
+                                        maxResults=50, singleEvents=True,
                                         orderBy='startTime').execute()
     events = events_result.get('items', [])
 
@@ -52,18 +60,34 @@ def main():
     for event in events:
         start = event['start'].get('dateTime', event['start'].get('date'))
         # formatted_start = start.
-        print(start, event['summary'])
+        print(start, event['summary'], file=fp)
 
-def prepare_files(date_generated):
+def open_file():
     cwd = os.getcwd()
     filename = "events"
-    file_p = open(cwd + '/' + filename + '.txt', 'w+')
+    beginning, _ = week_magic()
 
-    
+    file_p = open(cwd + '/' + filename + '_weekof_' + beginning.strftime("%x") + '.txt', 'w+')
+    return file_p
+def close_file(fp):
+    fp.close()
+
+def week_magic():
+    day = datetime.datetime.now()
+    day_of_week = day.weekday()
+
+    to_beginning_of_week = datetime.timedelta(days=day_of_week)
+    beginning_of_week = day - to_beginning_of_week
+
+    to_end_of_week = datetime.timedelta(days=6 - day_of_week)
+    end_of_week = day + to_end_of_week
+
+    return (beginning_of_week, end_of_week)
 
 if __name__ == '__main__':
-    prepare_files()
-    main()
+    fp = open_file()
+    main(fp)
+    close_file(fp)
 
 '''
 NOTES:
--- Python Scripts/pie_chart_playground/date_playground.py
+++ Python Scripts/pie_chart_playground/date_playground.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 
 import datetime
-
+import os
+#%%
+'''
 def main():
     today = datetime.datetime.now()
     beginning,end = week_magic(today)
     print("beginning of the week: {} \n end of the week: {}".format(beginning,end))
+    print("beginning of the week (Monday): {} \n end of the week (Sunday): {}".format(beginning.strftime("%x"),end))
+'''
 
 def week_magic(day):
     day_of_week = day.weekday()
@@ -18,5 +22,17 @@ def week_magic(day):
 
     return (beginning_of_week, end_of_week)
 
+#%%
+def main():
+    fp = open_files()
+    close_files(fp)
+def close_files(fp):
+    print("closing fp: {}".format(fp))
+    fp.close()
+def open_files():
+    cwd = os.getcwd()
+    filename = "test_open"
+    file_p = open(cwd + '/' + filename + '.txt', 'w+')
+    return file_p
 if __name__ == "__main__":
     main()
