9b63c4139e9ae437b36c6d328eb042f699a156d6
Nayman Leung <naymanl2@illinois.edu>
2020-12-31 18:25:16 -0500

    cpp gdb debugger side thing

---
+++ CPP Programs/linux_debugger/debug_main.cpp
@@ -0,0 +1,42 @@
+#include <sys/ptrace.h>
+
+class debugger {
+public:
+    debugger (std::string prog_name, pid_t pid)
+        : m_prog_name{std::move(prog_name)}, m_pid{pid} {}
+
+    void run();
+
+private:
+    std::string m_prog_name;
+    pid_t m_pid;
+};
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cerr << "Program name not specified";
+        return -1;
+    }
+
+    auto prog = argv[1];
+
+    auto pid = fork();
+    if (pid == 0) {
+        //we're in the child process
+        //execute debugee
+        //ptrace signature long ptrace(enum __ptrace_request request, pid_t pid,
+        // void *addr, void *data);
+        ptrace(PTRACE_TRACEME, 0, nullptr, nullptr);
+        execl(prog, prog, nullptr);
+
+    }
+    else if (pid >= 1)  {
+        //we're in the parent process
+        //execute debugger
+        //parent
+        std::cout << "Started debugging process " << pid << '\n';
+        debugger dbg{prog, pid};
+        dbg.run();
+    }
+
+}
